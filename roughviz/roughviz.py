2ef7ddecfc6608f52ea74089b5a0d790fd9db44c
Hannan Satopay <sathannan@hotmail.com>
2019-11-21 16:07:06 +0530

    Update roughviz.py

--- roughviz/roughviz.py
+++ roughviz/roughviz.py
@@ -8,7 +8,6 @@ import pkgutil
 def generate_template(data, labels, values, **kwargs):
     template = Template(data.decode("utf-8"))
     id_name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
-    print(labels)
     output = template.render(id_name = id_name,
                              labels = labels.tolist(),
                              values = values.tolist(),
@@ -17,16 +16,16 @@ def generate_template(data, labels, values, **kwargs):
 
 def bar(labels, values, **kwargs):
     data = pkgutil.get_data(__package__, 'templates/bar.html')
-    generate_template(data, labels, values, **kwargs)
+    return generate_template(data, labels, values, **kwargs)
 
 def barh(labels, values, **kwargs):
     data = pkgutil.get_data(__package__, 'templates/barh.html')
-    generate_template(data, labels, values, **kwargs)
+    return generate_template(data, labels, values, **kwargs)
 
 def pie(labels, values, **kwargs):
     data = pkgutil.get_data(__package__, 'templates/pie.html')
-    generate_template(data, labels, values, **kwargs)
+    return generate_template(data, labels, values, **kwargs)
 
 def donut(labels, values, **kwargs):
     data = pkgutil.get_data(__package__, 'templates/donut.html')
-    generate_template(data, labels, values, **kwargs)
+    return generate_template(data, labels, values, **kwargs)
